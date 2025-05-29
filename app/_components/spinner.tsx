f1d54a89bff5ebfa54e1d034a09c2786538115c1
bohdan <mota82589@gmail.com>
2025-05-29 17:13:52 +0300

    final

---
+++ app/_components/spinner.tsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <div className="h-12 w-12 animate-spin rounded-full border-4 border-blue-500 border-t-transparent" />
+    </div>
+  );
+}
